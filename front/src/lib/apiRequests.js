f211febf60716807afccd15b8bc9e176ad307e9c
Srivatsa19 <srivatsapotdar19@gmail.com>
2024-07-18 19:44:25 +0530

    Changed the deployed urls

--- front/src/lib/apiRequests.js
+++ front/src/lib/apiRequests.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequests = axios.create({
     // baseURL: "http://localhost:5000/api",
-    baseURL: "https://estate-api-phi.vercel.app",
+    baseURL: "http://estate-api-phi.vercel.app",
     withCredentials: true
 })
 
