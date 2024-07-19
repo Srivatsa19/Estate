80bb88f4a4fe756867682116cb227a7585a3a58c
Srivatsa19 <srivatsapotdar19@gmail.com>
2024-07-19 21:52:59 +0530

    Updated the deployed urls

--- front/src/lib/apiRequests.js
+++ front/src/lib/apiRequests.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequests = axios.create({
     // baseURL: "http://localhost:5000/api",
-    baseURL: "https://estate-api-phi.vercel.app",
+    baseURL: "https://estate-api-1kk8.onrender.com",
     withCredentials: true
 })
 
