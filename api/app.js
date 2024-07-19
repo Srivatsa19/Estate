4ba4b599f298ae3d4efd18b1c3d8aecbeca42bee
Srivatsa19 <srivatsapotdar19@gmail.com>
2024-07-19 20:27:14 +0530

    Added a port number in the .env file

--- api/app.js
+++ api/app.js
@@ -11,6 +11,7 @@ import messageRoute from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use(express.json());
@@ -23,6 +24,6 @@ app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/auth/test", testRoute);
 
-app.listen(5000, () => {
-    console.log("Server is running");
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
