af339c5cf3d8219563eab8dee7b519323c5780fc
Srivatsa19 <srivatsapotdar19@gmail.com>
2024-07-19 21:35:13 +0530

    Updated the vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 4173,
+    host: process.env.HOST || '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173,
   },
   preview: {
-    host: '0.0.0.0',
-    port: 4173,
+    host: process.env.HOST || '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173,
   },
-})
+});
