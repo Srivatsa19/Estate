8fb9e95f9f665b70149fab07f16d182e8516f54c
Srivatsa19 <srivatsapotdar19@gmail.com>
2024-08-01 21:25:07 +0530

    Modified app.js for deploying

--- api/app.js
+++ api/app.js
@@ -1,35 +1,38 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express from "express";
-import cors from "cors";
-import postRoute from "./routes/post.route.js";
-import authRoute from "./routes/auth.route.js";
-import testRoute from "./routes/test.route.js";
-import userRoute from "./routes/user.route.js";
-import chatRoute from "./routes/chat.route.js";
-import messageRoute from "./routes/message.route.js";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import cors from 'cors';
+import postRoute from './routes/post.route.js';
+import authRoute from './routes/auth.route.js';
+import testRoute from './routes/test.route.js';
+import userRoute from './routes/user.route.js';
+import chatRoute from './routes/chat.route.js';
+import messageRoute from './routes/message.route.js';
+import cookieParser from 'cookie-parser';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-
-app.use("/api/posts", postRoute);
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/chats", chatRoute);
-app.use("/api/messages", messageRoute);
-app.use("/api/auth/test", testRoute);
+app.use('/api/posts', postRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/chats', chatRoute);
+app.use('/api/messages', messageRoute);
+app.use('/api/auth/test', testRoute);
 
 app.use(express.static(path.join(__dirname, 'front', 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/front/build/index.html'));
+  res.sendFile(path.join(__dirname, 'front', 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
