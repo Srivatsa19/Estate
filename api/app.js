f2c8596499526ec053198e320660e09a14df37bd
Srivatsa19 <srivatsapotdar19@gmail.com>
2024-08-01 21:16:11 +0530

    Modified app.js for deploying

--- api/app.js
+++ api/app.js
@@ -9,6 +9,7 @@ import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,6 +18,7 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
@@ -24,6 +26,12 @@ app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/auth/test", testRoute);
 
+app.use(express.static(path.join(__dirname, 'front', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/front/build/index.html'));
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  console.log(`Server is running on port ${PORT}`);
+});
